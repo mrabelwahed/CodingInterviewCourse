409145f24fb441aa77f2b4b7a8931f7558bb5398
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2021-03-16 18:42:43 +0200

    add bubble and insertion sort

--- src/educative/algo/sortsearch/Helper.java
+++ src/educative/algo/sortsearch/Helper.java
@@ -30,12 +30,3 @@ class Helper {
     }
 }
 
-/** notes
- * this algorithm devides the array into two parts sorted & unsorted
- * after that it keeps search for min element in the array
- * and swap it with ith element in the array
- *Time complexity #
- * The time complexity of this code is in O(n^2)
- * O(n​2) because findMin() iterates over the entire array for each element of the given array.
- * The quadratic time complexity makes it impractical for large inputs.
- */
--- src/educative/algo/sortsearch/Sorting.java
+++ src/educative/algo/sortsearch/Sorting.java
@@ -5,9 +5,9 @@ public class Sorting {
     public static void main(String[] args) {
         int arr[] = {5,4,1,0,5,95,4,-100,200,0};
         int arrSize = 10;
-        selectionSort(arr, arrSize);
-        helper.printArray(arr, arrSize);
-
+//        selectionSort(arr, arrSize);
+         insertionSort(arr, arrSize);
+         helper.printArray(arr, arrSize);
     }
 
     public static void selectionSort(int arr[] , int len){
@@ -18,4 +18,68 @@ public class Sorting {
             arr[minIndex] = tmp;
         }
     }
+
+    public static void bubbleSort(int arr[] , int len){
+        for (int i = 0; i < len -1 ; i++)
+            // Last i elements are already in place
+            for (int j = 0; j < len-i-1; j++)
+                if (arr[j]>arr[j+1])
+                {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+    }
+
+    static void insertionSort(int[] arr, int len) {
+        int j=0;
+        for (int i = 0; i < len ; i++) {
+            j=i;
+                while(j>0 && arr[j]<arr[j-1]){
+                int tmp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tmp;
+                j--;
+            }
+
+        }
+
+    }
 }
+
+//notes
+/**
+ * selection sort algorithm
+ * divides the array into two parts sorted & unsorted
+ * after that it keeps search for min element in the array
+ * and swap it with ith element in the array
+ *
+ * Time complexity #
+ * The time complexity of this code is in O(n^2)
+ * O(n​2) because findMin() iterates over the entire array for each element of the given array.
+ * The quadratic time complexity makes it impractical for large inputs.
+ */
+
+/** bubble sort algorithm
+ *  compares every two adjacent pairs then swap them if they are in wrong order
+ *  Time complexity #
+ * The time complexity of the above code is in O(n^2)
+ * O(n​2). Again, this algorithm is not very efficient.
+ */
+
+/**
+ * insertion sort algorithm
+ * it is like when you sort your money . you pic the number that it should be sorted
+ * then you should shift other money to put it at the top of the money
+ * the shift process happens based on swapping
+ *
+ * Time complexity #
+ * The algorithm is in O(n^2)
+ * O(n2), which, again, makes it a poor choice for large input sizes.
+ * However, notice that the complexity is actually n^2
+ *
+ * ​​  only when the input array is sorted in reverse. So, the ‘best-case’ complexity (when the array is already sorted in the correct order) is \Omega(n)Ω(n).
+ *
+ * Space complexity #
+ * Also, note that all of these algorithms are in-place, hence their space complexity is in O(1).
+ */
